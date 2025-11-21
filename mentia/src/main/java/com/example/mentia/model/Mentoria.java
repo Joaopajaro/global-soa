e81ed7529b844d811ad923cf8674486baf424b72
Joaopajaro <114086775+Joaopajaro@users.noreply.github.com>
2025-11-21 13:35:37 -0300

    Update Mentoria.java

--- mentia/src/main/java/com/example/mentia/model/Mentoria.java
+++ mentia/src/main/java/com/example/mentia/model/Mentoria.java
@@ -20,7 +20,7 @@ public class Mentoria {
         this.descricao = descricao;
     }
 
-    // getters and setters
+ 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getTitulo() { return titulo; }
