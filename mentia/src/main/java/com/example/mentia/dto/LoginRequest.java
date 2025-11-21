948d1bcacb9ee313489a4e8fee333f8ad9dcde84
Joaopajaro <114086775+Joaopajaro@users.noreply.github.com>
2025-11-21 13:34:51 -0300

    Update LoginRequest.java

--- mentia/src/main/java/com/example/mentia/dto/LoginRequest.java
+++ mentia/src/main/java/com/example/mentia/dto/LoginRequest.java
@@ -9,7 +9,7 @@ public class LoginRequest {
     @NotBlank
     private String password;
 
-    // getters and setters
+   
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
     public String getPassword() { return password; }
