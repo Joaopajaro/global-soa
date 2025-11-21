8e16d9a44195daf812ff6bf8755e09ba505709f8
Joaopajaro <114086775+Joaopajaro@users.noreply.github.com>
2025-11-21 13:35:46 -0300

    Update Role.java

--- mentia/src/main/java/com/example/mentia/model/Role.java
+++ mentia/src/main/java/com/example/mentia/model/Role.java
@@ -19,7 +19,7 @@ public class Role {
         this.name = name;
     }
 
-    // getters and setters
+
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
     public ERole getName() { return name; }
